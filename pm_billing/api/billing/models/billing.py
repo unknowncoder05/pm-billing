62f36409590e04e4747eb30749141eadd715c7ca
unknowncoder05 <yersonlasso05@gmail.com>
2026-08-13 11:04:59 +0000

    chore: update shared template package

--- pm_billing/api/billing/models/billing.py
+++ pm_billing/api/billing/models/billing.py
@@ -32,12 +32,12 @@ class CreditTransaction(AbstractCreditTransaction):
         abstract = False
         app_label = 'pm_billing'
         indexes = [
-            models.Index(fields=['user', '-created_at']),
-            models.Index(fields=['transaction_type']),
-            models.Index(fields=['stripe_payment_intent_id']),
-            models.Index(fields=['stripe_checkout_session_id']),
-            models.Index(fields=['external_order_id']),
-            models.Index(fields=['context_type', 'context_id']),
+            models.Index(fields=['user', '-created_at'], name='pm_billing__user_id_39d832_idx'),
+            models.Index(fields=['transaction_type'], name='pm_billing__transac_994705_idx'),
+            models.Index(fields=['stripe_payment_intent_id'], name='pm_billing__stripe__c451c1_idx'),
+            models.Index(fields=['stripe_checkout_session_id'], name='pm_billing__stripe__a1277b_idx'),
+            models.Index(fields=['external_order_id'], name='pm_billing__externa_9677ed_idx'),
+            models.Index(fields=['context_type', 'context_id'], name='pm_billing__context_f3d273_idx'),
         ]
 
 
@@ -46,6 +46,6 @@ class PayPalCheckoutSession(AbstractPayPalCheckoutSession):
         abstract = False
         app_label = 'pm_billing'
         indexes = [
-            models.Index(fields=['user', '-created_at']),
-            models.Index(fields=['status']),
+            models.Index(fields=['user', '-created_at'], name='pm_billing__user_id_f4d297_idx'),
+            models.Index(fields=['status'], name='pm_billing__status_03363d_idx'),
         ]
